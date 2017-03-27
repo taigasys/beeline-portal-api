d446a3a30a93e70402bdb954dee13a7bc9a15264
Sergey Sidorenko <s.sidorenko@glavfinans.ru>
2017-03-27 09:27:02 +0300

    Добавлены дополнительные структуры, интерфейсы, методы

--- beelapi.go
+++ beelapi.go
@@ -41,3 +41,93 @@ type RecordInfo struct {
 	SaveDate      time.Time `xml:"-"`
 	Provider      string    `xml:"-"`
 }
+
+// GetRecord метод возвращает ифно об записей по индексу
+func (r *RecordInfos) GetRecord(i int64) *RecordInfo {
+	return &r.CallInfos[i]
+}
+
+// RecordInfos структура для хранения краткой информации о записях, необхадима для отправки запросо на получение файлов
+type ShortRecordInfos struct {
+	ShortCallInfos []ShortRecordInfo
+	Count          int
+}
+
+// RecordInfo структура для хранения краткой информации об отдельной записи
+type ShortRecordInfo struct {
+	RecordId int64
+	Status   string
+	SaveDate time.Time
+}
+
+// IRecordsInfoProvider интерфейс для отправки запросов на получение файлов разговоров
+type IRecordsInfoProvider interface {
+	Len() int64
+	GetRecordInfo(index int64) IRecordInfoProvider
+}
+
+// IRecordInfoProvider интерфейс для отправки запросов на получение файлов разговоров, хранит инфо об отдельной записи
+type IRecordInfoProvider interface {
+	GetId() int64
+	GetStatus() string
+	SetStatus(s string)
+}
+
+// Len метод расситывает количество записей
+func (r *RecordInfos) Len() int64 {
+	return int64(len(r.CallInfos))
+}
+
+// GetRecordInfo метод возвращает инфо об отдельной записи, в данном случае - полную
+func (r *RecordInfos) GetRecordInfo(index int64) IRecordInfoProvider {
+	return &r.CallInfos[index]
+}
+
+// GetId метод возвращает ID записи разговора
+func (r *RecordInfo) GetId() int64 {
+	return r.RecordId
+}
+
+// GetStatus метод возвращает статус хранения записи разговора
+func (r *RecordInfo) GetStatus() string {
+	return r.Status
+}
+
+// SetStatus метод устанавливает статус хранения записи разговора
+func (r *RecordInfo) SetStatus(s string) {
+	r.Status = s
+}
+
+// GetId метод возвращает ID записи разговора
+func (ri *ShortRecordInfo) GetId() int64 {
+	return ri.RecordId
+}
+
+// GetStatus метод возвращает статус хранения записи разговора
+func (ri *ShortRecordInfo) GetStatus() string {
+	return ri.Status
+}
+
+// SetStatus метод устанавливает статус хранения записи разговора
+func (ri *ShortRecordInfo) SetStatus(s string) {
+	ri.Status = s
+}
+
+// Len  метод расситывает количество записей
+func (ri *ShortRecordInfos) Len() int64 {
+	return int64(len(ri.ShortCallInfos))
+}
+
+// GetRecordInfo метод возвращает инфо об отдельной записи, в данном случае - краткую
+func (ri *ShortRecordInfos) GetRecordInfo(index int64) IRecordInfoProvider {
+	return &ri.ShortCallInfos[index]
+}
+
+//BAPIError Тип хранения ошибок
+type BAPIError struct {
+	Msg string
+}
+
+func (d BAPIError) Error() string {
+	return d.Msg
+}
